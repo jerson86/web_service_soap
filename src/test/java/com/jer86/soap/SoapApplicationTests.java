629cfbffbd99136c24b0065946e2262c83745c1d
Jerson Garcia <jersonjavier@gmail.com>
2024-08-07 13:15:18 -0500

    creacion y prueba del proyecto

---
+++ src/test/java/com/jer86/soap/SoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jer86.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
